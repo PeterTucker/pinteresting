5d60566d20063cc0d5ae06087a3ed4319e1f946a
bitnami <bitnami@ubuntu.defaultdomain>
2016-03-15 16:41:29 +0000

    Add pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,7 +7,8 @@
 
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show] # User must be authenticated to run actions, except viewing via seeing the entire pin list and individual pins via show.
   # GET /pins
   # GET /pins.json
   def index
@@ -22,7 +23,7 @@ class PinsController < ApplicationController
   # GET /pins/new
   # Shows page for actually creating a pin. Once submitted it links to the 'create'(POST) action.
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build # Creates a new pin which als has a user associated as opposed to Pin.new
   end
 
   # GET /pins/1/edit
@@ -33,7 +34,7 @@ class PinsController < ApplicationController
   # POST /pins.json
   # Actually create the pin as opposed to serving the create pin page('new' action)
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
       if @pin.save
         redirect_to @pin, notice: 'Pin was successfully created.'
@@ -59,12 +60,22 @@ class PinsController < ApplicationController
     redirect_to pins_url
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
-
+    
+    def correct_user
+      if user_signed_in?
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "You are not Authorized to edit this pin." if @pin.nil?
+      else
+      redirect_to pins_path, notice: "Sign in to edit a pin."
+      end
+    end
+    
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
